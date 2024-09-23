a2c4a41682cd704693bb9a52b02ca8f259a2e208
Evan <kuoevan145@gmail.com>
2024-09-22 18:57:14 -0700

    Added in images, need to fix ordering

--- AntichessUI.java
+++ AntichessUI.java
@@ -22,12 +22,23 @@ public class AntichessUI {
         frame.setSize(600, 600);
         JPanel panel = new JPanel(new GridLayout(8, 8));
 
+        loadImages();
+
         // Create board buttons and set up action listeners
         boardButtons = new JButton[8][8];
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 JButton button = new JButton();
                 boardButtons[row][col] = button;
+
+                if ((row + col) % 2 == 0) {
+                    button.setBackground(Color.WHITE); // Light square
+                } else {
+                    button.setBackground(Color.GRAY); // Dark square
+                }
+
+                button.setOpaque(true);
+                button.setBorderPainted(false);
 
                 // Add action listener to each button to handle user clicks
                 final int currentRow = row;
@@ -64,13 +75,31 @@ public class AntichessUI {
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 Piece piece = board[row][col];
-                if (piece != null) {
-                    boardButtons[row][col].setText(piece.getSymbol());
+                 if (piece != null) {
+                        int index = piece.getType().ordinal() + (piece.getColor() == Piece.Color.WHITE ? 0 : 6);
+                        boardButtons[row][col].setIcon(pieceImages[index]);
                 } else {
-                    boardButtons[row][col].setText("");
+                        boardButtons[row][col].setIcon(null); // Clear icon for empty squares
                 }
             }
         }
+    }
+
+    private ImageIcon[] pieceImages = new ImageIcon[12];
+
+    private void loadImages() {
+        pieceImages[0] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/WhitePawn.png");
+        pieceImages[1] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/WhiteRook.png");
+        pieceImages[2] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/WhiteKnight.png");
+        pieceImages[3] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/WhiteBishop.png");
+        pieceImages[4] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/WhiteQueen.png");
+        pieceImages[5] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/WhiteKing.png");
+        pieceImages[6] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/BlackPawn.png");
+        pieceImages[7] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/BlackRook.png");
+        pieceImages[8] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/BlackKnight.png");
+        pieceImages[9] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/BlackBishop.png");
+        pieceImages[10] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/BlackQueen.png");
+        pieceImages[11] = new ImageIcon("/Users/ekuo25/MyScripts/SSP_Anti-Chess/BlackKing.png");
     }
 
     public static void main(String[] args) {
